baac37e0ab0cdbff9646143270a38c93d6f9149a
KoalaOfficial <polypody3721green@gmail.com>
2025-07-16 22:10:41 +0200

    Create neural_test.cpp

---
+++ neural_test.cpp
@@ -0,0 +1,21 @@
+#include "neural_network.hpp"
+#include <iostream>
+
+int main() {
+    // XOR problem
+    NeuralNetwork nn({2, 2, 1});
+    std::vector<std::vector<double>> inputs = {
+        {0,0}, {0,1}, {1,0}, {1,1}
+    };
+    std::vector<std::vector<double>> targets = {
+        {0}, {1}, {1}, {0}
+    };
+    nn.train(inputs, targets, 0.5, 5000);
+
+    std::cout << "Testing XOR:\n";
+    for (size_t i = 0; i < inputs.size(); ++i) {
+        auto out = nn.predict(inputs[i]);
+        std::cout << inputs[i][0] << " xor " << inputs[i][1] << " = " << out[0] << "\n";
+    }
+    return 0;
+}
